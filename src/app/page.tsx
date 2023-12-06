c5ee9b3ce992c60a62018f7b956a6b1bd0193956
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-12-05 16:17:18 -0800

    added smythe text style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,17 @@ import Image from "next/image";
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { smythe } from "./ui/font";
 
 const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
+  { name: "About Us", href: "#" },
+  { name: "Products", href: "#" },
+  { name: "Our Team", href: "#" },
+  { name: "Recipes", href: "#" },
 ];
 
-export default function Example() {
-  <header />
+export default function SplashPage() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <div className="bg-white">
@@ -44,13 +45,13 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a
+              <Link
                 key={item.name}
                 href={item.href}
-                className="text-sm font-semibold leading-6 text-gray-900"
+                className={`${smythe.className} text-xl font-bold leading-6 text-gray-900`}
               >
                 {item.name}
-              </a>
+              </Link>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -71,14 +72,14 @@ export default function Example() {
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <img
                   className="h-8 w-auto"
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                   alt=""
                 />
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -128,38 +129,41 @@ export default function Example() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="flex-row mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
+              <Link href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
+              </Link>
+            </div> */}
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              Nioh Cha Essence
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Crafting Tranquility Through Authentic Japanese Tea
+            </p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Discover the art of tea-making, immerse yourself in mindful
+              rituals, and savor the serenity of Nihon Cha Essence where every
+              cup is an ode to the soul-soothing essence of Japanese tea.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
+              {/* <Link
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
-              </a>
-              <a
+              </Link> */}
+              <Link
                 href="#"
-                className="text-sm font-semibold leading-6 text-gray-900"
+                className="relative px-3.5 py-2.5 text-lg rounded-full ring-1 hover:ring-gray-900/20 font-semibold leading-6 text-gray-900"
               >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
+                Shop <span aria-hidden="true">→</span>
+              </Link>
             </div>
           </div>
         </div>
---
+++ src/app/ui/font.ts
@@ -0,0 +1,7 @@
+import { Inter, Smythe } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const smythe = Smythe({
+  weight: ["400"],
+  subsets: ["latin"],
+});
