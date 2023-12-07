70cb32125320381ae17a4d4f1f2c36135c8ed68c
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-12-07 11:18:00 -0800

    test 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { smythe } from "./ui/font";
+import { ArrowRight, Instagram, Linkedin, Twitter } from "lucide-react";
 
 const navigation = [
   { name: "About Us", href: "#" },
@@ -18,49 +19,64 @@ export default function SplashPage() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
+      <Image
+        className="absolute inset-0 object-cover w-full h-full"
+        src="/hero-desktop.jpeg"
+        alt=""
+        width={1000}
+        height={760}
+      />
+      <header className="border-b-2 border-gray-600 absolute inset-x-0 top-0 z-50">
         <nav
           className="flex items-center justify-between p-6 lg:px-8"
           aria-label="Global"
         >
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
+              <Image
+                src="/icon.png"
+                alt="Nioh Cha Essence"
+                width={50}
+                height={50}
               />
-            </a>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-yellow-500"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+          {/* Navbar */}
+          <div className="hidden lg:flex lg:gap-x-12 items-center">
             {navigation.map((item) => (
               <Link
                 key={item.name}
                 href={item.href}
-                className={`${smythe.className} text-xl font-bold leading-6 text-gray-900`}
+                className={`${smythe.className} bg-gray-600 px-5 py-2.5 rounded-full ring-1 hover:ring-blue-500 text-lg font-semibold leading-6 text-yellow-500`}
               >
                 {item.name}
               </Link>
             ))}
+            <div className="bg-gray-100"></div>
+            <Instagram className="h-6 w-6 text-yellow-500" aria-hidden="true" />
+            <Twitter className="h-6 w-6 text-yellow-500" aria-hidden="true" />
+            <Linkedin className="h-6 w-6 text-yellow-500" aria-hidden="true" />
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a
+            <Link
               href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
+              className={`${smythe.className} bg-gray-600 px-5 py-2.5 rounded-full ring-1 hover:ring-blue-500 text-lg font-semibold leading-6 text-yellow-500`}
             >
+              {/* Log in
+            <ArrowRight /> */}
               Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+            </Link>
           </div>
         </nav>
         <Dialog
@@ -74,10 +90,11 @@ export default function SplashPage() {
             <div className="flex items-center justify-between">
               <Link href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
+                <Image
+                  src="/icon.png"
+                  alt="Nioh Cha Essence"
+                  width={50}
+                  height={50}
                 />
               </Link>
               <button
@@ -89,46 +106,39 @@ export default function SplashPage() {
                 <XMarkIcon className="h-6 w-6" aria-hidden="true" />
               </button>
             </div>
+            {/* mobile navbar */}
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
                       href={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
                 <div className="py-6">
-                  <a
+                  <Link
                     href="#"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
           </Dialog.Panel>
         </Dialog>
       </header>
-
+      {/* splash page */}
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div>
+        ></div>
         <div className="flex-row mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -139,14 +149,20 @@ export default function SplashPage() {
               </Link>
             </div> */}
           </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <div className=" text-center ">
+            <h1
+              className={`${smythe.className} text-4xl font-bold tracking-tight text-yellow-500 sm:text-6xl`}
+            >
               Nioh Cha Essence
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p
+              className={`${smythe.className} mt-6 text-lg leading-8 text-yellow-500`}
+            >
               Crafting Tranquility Through Authentic Japanese Tea
             </p>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p
+              className={`${smythe.className} mt-6 text-lg leading-8 text-yellow-500`}
+            >
               Discover the art of tea-making, immerse yourself in mindful
               rituals, and savor the serenity of Nihon Cha Essence where every
               cup is an ode to the soul-soothing essence of Japanese tea.
@@ -160,7 +176,7 @@ export default function SplashPage() {
               </Link> */}
               <Link
                 href="#"
-                className="relative px-3.5 py-2.5 text-lg rounded-full ring-1 hover:ring-gray-900/20 font-semibold leading-6 text-gray-900"
+                className={`${smythe.className} bg-gray-600 px-20 py-2.5 rounded-full ring-1 hover:ring-blue-500 text-2xl font-semibold leading-6 text-yellow-500`}
               >
                 Shop <span aria-hidden="true">→</span>
               </Link>
@@ -170,15 +186,7 @@ export default function SplashPage() {
         <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div>
+        ></div>
       </div>
     </div>
   );
---
+++ src/app/ui/Icons.tsx
@@ -0,0 +1,14 @@
+import { ArrowRight, Instagram, Linkedin, Twitter } from "lucide-react";
+
+const App = () => {
+  return (
+    <div>
+      <Instagram />
+      <Twitter />
+      <Linkedin />
+      <ArrowRight />
+    </div>
+  );
+};
+
+export default App;
